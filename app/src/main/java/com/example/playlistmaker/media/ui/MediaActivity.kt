868289a866b213fd074bf33942e796759fd1b78b
julia.vakulina <yuliavak@mail.ru>
2024-06-18 21:36:39 +0700

    sprint 19 - commit 1. Use Single activity and bottom navigation

--- app/src/main/java/com/example/playlistmaker/media/ui/MediaActivity.kt
+++ app/src/main/java/com/example/playlistmaker/media/ui/MediaActivity.kt
@@ -7,42 +7,41 @@ import android.widget.Button
 import androidx.fragment.app.add
 import androidx.fragment.app.commit
 import com.example.playlistmaker.R
-import com.example.playlistmaker.databinding.ActivityMediaBinding
 import com.google.android.material.tabs.TabLayoutMediator
 
 class MediaActivity : AppCompatActivity() {
-    private lateinit var binding: ActivityMediaBinding
-    private lateinit var tabMediator: TabLayoutMediator
+    //private lateinit var binding: ActivityMediaBinding
+    //private lateinit var tabMediator: TabLayoutMediator
     @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = ActivityMediaBinding.inflate(layoutInflater)
-        setContentView(binding.root)
+        //binding = ActivityMediaBinding.inflate(layoutInflater)
+        //setContentView(binding.root)
 
-        binding.viewPager.adapter = MediaViewPagerAdapter(supportFragmentManager, lifecycle)
+        //binding.viewPager.adapter = MediaViewPagerAdapter(supportFragmentManager, lifecycle)
 
-        tabMediator = TabLayoutMediator(binding.tabLayout, binding.viewPager) { tab, position ->
-            when (position) {
-                0 -> tab.text = "Избранные треки"
-                1 -> tab.text = "Плейлисты"
-            }
-        }
-        tabMediator.attach()
+        //tabMediator = TabLayoutMediator(binding.tabLayout, binding.viewPager) { tab, position ->
+        //    when (position) {
+        //        0 -> tab.text = "Избранные треки"
+        //        1 -> tab.text = "Плейлисты"
+        //    }
+        //}
+        //tabMediator.attach()
 
         //    if (savedInstanceState == null) {
         //    supportFragmentManager.commit {
         //        add<FavoritesFragment>(R.id.fragment_container_view)
         //    }
         //}
-        val buttonLeft = findViewById<Button>(R.id.button_left_media)
-        buttonLeft.setOnClickListener {
-            this.onBackPressed()
-        }
+        //val buttonLeft = findViewById<Button>(R.id.button_left_media)
+        //buttonLeft.setOnClickListener {
+        //    this.onBackPressed()
+        //}
 
     }
 
     override fun onDestroy() {
         super.onDestroy()
-        tabMediator.detach()
+    //    tabMediator.detach()
     }
 }
